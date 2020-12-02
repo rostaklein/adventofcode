26fc8b492c93c819663558565ef5d6b8b8b89079
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-12-02 23:22:18 +0100

    day 2 part 2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -7,14 +7,19 @@ const fileLines = file.split("\n");
 const getIsValidGroup = (params: {
   from: number;
   to: number;
-  letter?: string;
-  password?: string;
+  letter: string;
+  password: string;
 }) => {
-  const matches = params.password?.match(new RegExp(`${params.letter}`, "gi"));
-  const matchesTimes = matches?.length ?? 0;
-  if (matchesTimes >= params.from && matchesTimes <= params.to) {
-    return true;
-  }
+  const charAtFrom = params.password.charAt(params.from - 1);
+  const charAtTo = params.password.charAt(params.to - 1);
+
+  const firstMatches = charAtFrom === params.letter;
+  const secondMatches = charAtTo === params.letter;
+  return {
+    ...params,
+    firstMatches,
+    secondMatches,
+  };
 };
 
 export const getValidPasswords = () => {
@@ -26,11 +31,15 @@ export const getValidPasswords = () => {
     .map((item) => ({
       from: Number(item?.from),
       to: Number(item?.to),
-      letter: item?.letter,
-      password: item?.password,
+      letter: item?.letter ?? "",
+      password: item?.password ?? "",
     }));
 
-  const validGroups = groups.filter(getIsValidGroup);
+  const validGroups = groups
+    .map(getIsValidGroup)
+    .filter(({ firstMatches, secondMatches }) => {
+      return firstMatches !== secondMatches;
+    });
 
   return validGroups;
 };
