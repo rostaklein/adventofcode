6039f4fe2f43133438a3ca4f4b14dcc49c86830b
Rosta Klein <rostaklein@gmail.com>
2020-12-03 22:30:35 +0100

    day 2 part 2

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -4,20 +4,28 @@ import path from "path";
 const file = fs.readFileSync(path.resolve(__dirname, "./data.txt")).toString();
 const fileLines = file.split("\r\n");
 
-export const main = (): number => {
+export const traverseMap = (right: number, down: number): number => {
   let totalTrees = 0;
 
-  fileLines.shift();
+  let currentPosition = right;
+  for (let i = down; i < fileLines.length; i += down) {
+    const letterPosition = currentPosition % fileLines[i].length;
 
-  let currentIndex = 1;
-  for (const line of fileLines) {
-    const letterPosition = (3 * currentIndex) % line.length;
-
-    if (line.charAt(letterPosition) === "#") {
+    if (fileLines[i].charAt(letterPosition) === "#") {
       totalTrees++;
     }
-    currentIndex++;
+    currentPosition += right;
   }
 
   return totalTrees;
+};
+
+export const main = (): number => {
+  return (
+    traverseMap(1, 1) *
+    traverseMap(3, 1) *
+    traverseMap(5, 1) *
+    traverseMap(7, 1) *
+    traverseMap(1, 2)
+  );
 };
