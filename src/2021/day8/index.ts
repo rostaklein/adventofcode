a72be3b5c49d3d4fefdafddcd1a43a9061295b38
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-12-08 08:21:47 +0100

    determining digits

--- src/2021/day8/index.ts
+++ src/2021/day8/index.ts
@@ -3,29 +3,163 @@ import { readLinesFromAFile } from "../utils";
 const fileLines = readLinesFromAFile("./day8/data.txt");
 
 export class SevenSegmentSearch {
-  private lengthsToPick = [2, 4, 3, 7];
+  private stringToNumberMap = new Map<number, string>();
   private outputValues: string[][];
-  constructor(input: string[]) {
-    this.outputValues = input.map((item) => {
-      const [, output] = item.split(" | ");
+  private inputValues: string[][];
 
-      return output.split(" ");
+  constructor(input: string[]) {
+    this.inputValues = [];
+    this.outputValues = [];
+    input.forEach((item) => {
+      const [inp, output] = item.split(" | ");
+
+      this.inputValues.push(inp.split(" "));
+      this.outputValues.push(output.split(" "));
     });
+  }
+
+  private determineTopRight(input: string[]) {
+    const eight = this.stringToNumberMap.get(8);
+    const six = this.stringToNumberMap.get(6);
+
+    const topRightPart = eight
+      ?.split("")
+      .filter((letter) => !six?.split("").includes(letter));
+
+    return topRightPart![0];
+  }
+
+  private getEasyNumbers(strings: string[]) {
+    for (const string of strings) {
+      switch (string.length) {
+        case 2:
+          this.stringToNumberMap.set(1, string);
+          break;
+        case 3:
+          this.stringToNumberMap.set(7, string);
+          break;
+        case 4:
+          this.stringToNumberMap.set(4, string);
+          break;
+        case 7:
+          this.stringToNumberMap.set(8, string);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  private determineNine(input: string[]) {
+    const four = this.stringToNumberMap.get(4);
+    const nine = input.find(
+      (inp) => inp.length === 6 && this.includesAllSubstringLetters(inp, four!)
+    );
+
+    this.stringToNumberMap.set(9, nine!);
+  }
+
+  private determineZero(input: string[]) {
+    const remaining = this.filterOutRemaining(input);
+
+    const one = this.stringToNumberMap.get(1);
+
+    const zero = remaining.find(
+      (inp) => inp.length === 6 && this.includesAllSubstringLetters(inp, one!)
+    );
+
+    this.stringToNumberMap.set(0, zero!);
+  }
+
+  private determineSix(input: string[]) {
+    const remaining = this.filterOutRemaining(input);
+
+    const six = remaining.find((inp) => inp.length === 6);
+
+    this.stringToNumberMap.set(6, six!);
+  }
+
+  private determineThree(input: string[]) {
+    const remaining = this.filterOutRemaining(input);
+
+    const seven = this.stringToNumberMap.get(7);
+
+    const three = remaining.find(
+      (inp) => inp.length === 5 && this.includesAllSubstringLetters(inp, seven!)
+    );
+
+    this.stringToNumberMap.set(3, three!);
+  }
+
+  private determineTwo(input: string[], topRight: string) {
+    const remaining = this.filterOutRemaining(input);
+
+    const two = remaining.find((str) => str.includes(topRight))!;
+
+    this.stringToNumberMap.set(2, two!);
+  }
+
+  private determineFive(input: string[]) {
+    const remaining = this.filterOutRemaining(input);
+
+    const five = remaining[0];
+
+    this.stringToNumberMap.set(5, five);
   }
 
   public run() {
     let count = 0;
 
-    for (const output of this.outputValues) {
-      const hasSome = output.filter((item) =>
-        this.lengthsToPick.includes(item.length)
-      );
+    for (const row of this.inputValues) {
+      this.getEasyNumbers(row);
+      this.determineNine(row);
+      this.determineZero(row);
+      this.determineSix(row);
+      this.determineThree(row);
 
-      count += hasSome.length;
-      // console.log({ hasSome });
+      const topRightPart = this.determineTopRight(row);
+      this.determineTwo(row, topRightPart);
+      this.determineFive(row);
     }
 
+    console.log(this.stringToNumberMap);
+
     return count;
+  }
+
+  private filterOutRemaining(strings: string[]) {
+    return strings.filter((str) => {
+      return ![...this.stringToNumberMap.values()].includes(str);
+    });
+  }
+
+  private includesAllSubstringLetters(string: string, substring: string) {
+    for (const letter of substring) {
+      if (!string.includes(letter)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private permut(string: string): string[] | string {
+    if (string.length < 2) return string;
+    var permutations = []; // This array will hold our permutations
+    for (var i = 0; i < string.length; i++) {
+      var char = string[i];
+
+      // Cause we don't want any duplicates:
+      if (string.indexOf(char) != i)
+        // if char was used already
+        continue; // skip it this time
+
+      var remainingString =
+        string.slice(0, i) + string.slice(i + 1, string.length); //Note: you can concat Strings via '+' in JS
+
+      for (var subPermutation of this.permut(remainingString))
+        permutations.push(char + subPermutation);
+    }
+    return permutations;
   }
 }
 
