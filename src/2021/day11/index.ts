caa98d674fa9c9d2071e55795ff3de51a7b0e8b8
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-12-11 10:06:02 +0100

    small example done

--- src/2021/day11/index.ts
+++ src/2021/day11/index.ts
@@ -15,7 +15,13 @@ export class OctopusFlasher {
   }
 
   public getBoardAfterXSteps(steps: number) {
-    this.board = this.incrementAllBoardByOne(this.board);
+    for (let i = 0; i < steps; i++) {
+      this.incrementAllBoardByOne(this.board);
+      while (this.checkIfHasTens(this.board)) {
+        this.flashTens(this.board);
+      }
+    }
+
     return this.stringifyBoard(this.board);
   }
 
@@ -30,7 +36,52 @@ export class OctopusFlasher {
       modifiedBoard.push(modifiedRow);
     }
 
-    return modifiedBoard;
+    this.board = modifiedBoard;
+  }
+
+  private checkIfHasTens(board: Board) {
+    for (const row of board) {
+      if (row.some((num) => num === 10)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private flashTens(board: Board) {
+    let y = 0;
+    for (const row of board) {
+      let x = 0;
+      for (const num of row) {
+        if (num === 10) {
+          console.log({ num, x, y });
+          this.board[y][x] = 0;
+          this.incrementAllAround(x, y);
+          debugger;
+        }
+        x++;
+      }
+      y++;
+    }
+  }
+
+  private incrementAllAround(x: number, y: number) {
+    for (const xOffset of [-1, 0, 1]) {
+      for (const yOffset of [-1, 0, 1]) {
+        const xToPop = x + xOffset;
+        const yToPop = y + yOffset;
+
+        let point = null;
+        try {
+          point = this.board[yToPop][xToPop];
+        } catch (err) {}
+
+        if (point !== null && point <= 9 && point !== 0) {
+          console.log({ point });
+          this.board[yToPop][xToPop] = point + 1;
+        }
+      }
+    }
   }
 
   private stringifyBoard(board: Board) {
